9071d8f359dd2f5d68926771b441046644ca78b1
Serge Rosmorduc <serge.rosmorduc@cnam.fr>
2026-03-18 17:54:29 +0100

    renaming

---
+++ editor-jgoodies/src/main/java/tableditor/StudentUIBuilder.java
@@ -0,0 +1,36 @@
+package tableditor;
+
+import tableditor.control.StudentCreatorController;
+import tableditor.control.StudentNavigatorController;
+import tableditor.control.StudentTableController;
+import tableditor.ui.JStudentBase;
+import tableditor.ui.specs.IStudentBase;
+import tableditor.viewmodel.StudentCreationViewModel;
+import tableditor.viewmodel.StudentNavigatorViewModel;
+import tableditor.viewmodel.StudentSharedViewModel;
+import tableditor.viewmodel.StudentTableViewModel;
+
+/**
+ * Builder for the student UI.
+ */
+public class StudentUIBuilder {
+
+    private StudentSharedViewModel sharedViewModel;
+    private IStudentBase ui = new JStudentBase();
+    private StudentTableController tablePresenter;
+    private StudentCreatorController studentCreatorPresenter;
+    private StudentNavigatorController studentNavigatorPresenter;
+
+    public StudentUIBuilder(StudentSharedViewModel studentSharedViewModel) {        
+        this.sharedViewModel = studentSharedViewModel;
+        this.tablePresenter = new StudentTableController(new StudentTableViewModel(studentSharedViewModel), ui.getStudentTable());    
+        this.studentCreatorPresenter = new StudentCreatorController(new StudentCreationViewModel(studentSharedViewModel), ui.getStudentCreatorPanel());
+        this.studentNavigatorPresenter = new StudentNavigatorController(new StudentNavigatorViewModel(studentSharedViewModel), ui.getStudentNavigatorPanel());
+    }   
+
+    public void display() {
+        ui.setVisible(true);
+        ui.setDefaultCloseOperation(JStudentBase.EXIT_ON_CLOSE);
+    }
+    
+}
